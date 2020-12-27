ddd1ad6634a39eb26fa9e5f74b0f44216caeb0e2
HaninAyadi <Ayadi.Hanin@insat.u-carthage.tn>
2020-12-27 12:29:20 +0100

    added sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    SidebarComponent,
     NotFoundComponent,
   ],
   imports: [
