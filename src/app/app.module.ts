e4d829923664a4449691f6c643ede125d74850c9
PsycoCast <gabsiyassine@gmail.com>
2021-01-21 23:51:15 +0100

    fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent,
     NotFoundComponent,
   ],
   imports: [
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
+import {AuthenticationRoutingModule} from './authentication-routing.module';
 
 
 
 @NgModule({
   declarations: [SigninComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthenticationRoutingModule
   ]
 })
 export class AuthenticationModule { }
