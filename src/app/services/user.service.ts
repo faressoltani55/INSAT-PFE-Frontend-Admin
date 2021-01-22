4dce6b860db953827d6559df289198923d91f2e0
PsycoCast <gabsiyassine@gmail.com>
2021-01-22 01:33:54 +0100

    authentication service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Utils} from '../utils/utils';
+import {HttpClient} from '@angular/common/http';
+import {Admin} from '../utils/models/Admin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  public login(username: string, password: string): Observable<LoginResponse> {
+    return this.http.post(Utils.baseUrl + '/auth', {
+      username: username,
+      password: password,
+    }, ) as Observable<LoginResponse>;
+  }
+
+  public getUser(): Observable<Admin> {
+    return this.http.get(Utils.baseUrl + '/admin') as Observable<Admin>;
+  }
+
+
+}
+
+class LoginResponse {
+  public access: string;
+  public refresh: string;
+}
+
