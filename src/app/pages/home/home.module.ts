f7c3c01b1c65130c2ef647426367f534b9518bbf
PsycoCast <gabsiyassine@gmail.com>
2021-02-03 12:18:11 +0100

    adding a spinner

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import {HomeComponent} from './home.component';
+import {SpinnerModule} from '../../components/spinner/spinner.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SpinnerModule
   ]
 })
 export class HomeModule { }
