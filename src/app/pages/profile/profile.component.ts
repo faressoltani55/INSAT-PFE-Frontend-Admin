5e1341908227a861fcb2a11d76eda509c900727a
PsycoCast <gabsiyassine@gmail.com>
2021-02-07 18:33:03 +0100

    profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {StudentService} from '../../services/student.service';
+import {Address} from '../../utils/models/Adress';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+
+  id = '6014276c273a2a3f58be90a4';
+
+  constructor(private studentService: StudentService) { }
 
   ngOnInit(): void {
+    this.studentService.getStudentById(this.id).subscribe( data => {
+      this.profile = data;
+      if (! this.profile.address){
+        this.profile.address = new Address();
+      }
+      console.log(data);
+    });
   }
+
+  submit(): void{
+    this.studentService.updateStudent(this.id, this.profile).subscribe();
+  }
+
 
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {BASE_URL} from "./CONSTS";
 import {Student} from "../utils/models/Student";
+import {Observable} from 'rxjs';
+import {Soutenance} from '../utils/models/Soutenance';
+import {Utils} from '../utils/utils';
 
 const URI = "students/"
 @Injectable({
@@ -10,6 +13,15 @@ const URI = "students/"
 export class StudentService {
 
   constructor(private http: HttpClient) { }
+
+  public getStudentById(id: string): Observable<Student> {
+    return this.http.get(Utils.baseUrl + '/students/' + id ) as Observable<Student>;
+  }
+
+  public updateStudent(id, body): Observable<Student> {
+    return this.http.put(Utils.baseUrl + '/students/' + id, body ) as Observable<Student>;
+  }
+
 
   get() {
     return this.http.get<Student[]>(BASE_URL + URI);
